6e2e4187118c415e896ec2388070abba51a8ca67
Ashley Radford <aradford7@outlook.com>
2019-08-04 20:46:22 -0700

    add footer to contacts

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -17658,7 +17658,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51209" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52446" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
